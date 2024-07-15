966d589f4ee81c3f0c8734347526d14ee8a55f7e
Hir0su <owen.santoscoc@gmail.com>
2024-07-15 10:42:55 +0800

    2nd update

---
+++ JS/main.js
@@ -0,0 +1,30 @@
+/*=========================================================== Toggle Icon Navbar ===========================================================*/
+
+let nemuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+
+/*=========================================================== Scroll Section Active Link ===========================================================*/
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navlinks.forEach.apply(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+}
