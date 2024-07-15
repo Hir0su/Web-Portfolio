9d5dfa7060285a34e0f8101d6d8e2a9a4c5ac203
Hir0su <owen.santoscoc@gmail.com>
2024-07-15 18:52:44 +0800

    3rd commit

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,17 @@
 /*=========================================================== Toggle Icon Navbar ===========================================================*/
 
-let nemuIcon = document.querySelector('#menu-icon');
+let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('fa-xmark');
-    navbar.classList.toggle('active');
+    navbar.classList.toggle('active')
 }
 
 /*=========================================================== Scroll Section Active Link ===========================================================*/
 
 let sections = document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('header nav a');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
@@ -21,10 +21,21 @@ window.onscroll = () => {
         let id = sec.getAttribute('id');
 
         if(top >= offset && top < offset + height) {
-            navlinks.forEach.apply(links => {
+            navLinks.forEach.apply(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
             });
         };
     });
-}
+
+
+    /*=========================================================== Sticky Navbar ===========================================================*/
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*=========================================================== Remove Toggle Icon and Navbar ===========================================================*/
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+
+/*=========================================================== Sticky Navbar ===========================================================*/
