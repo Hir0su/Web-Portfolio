7f664f065af9d9279f24cc4e193544a169ce5ab6
Hir0su <owen.santoscoc@gmail.com>
2024-07-22 02:21:02 +0800

    Added new update
    Modal boxes for the portfolio and UI updates for mobile

--- JS/main.js
+++ JS/main.js
@@ -88,8 +88,38 @@ const projectDetails = {
         title: "Hey Whizzy",
         description: "Hey Whizzy is an innovative interactive voice-assistive kiosk designed to enhance information accessibility and engagement at Mapúa Malayan Colleges Laguna. By leveraging advanced technologies like Raspberry Pi, Flask API, and MySQL, it provides real-time, organized information to students and visitors, making campus navigation and information retrieval seamless and efficient.",
         github: "https://github.com/Hir0su/Hey-Whizzy",
-        slides: "https://docs.google.com/presentation/d/e/2PACX-1vTjWYyZhVQp38gvp7lmxTewmg5NWvKaZtBzNR_50Vq20MWDDR4bQihYWiMTJLx7v9Ez8fGJ7c2atZzJ/embed?start=true&loop=true&delayms=2000",
+        slides: "https://docs.google.com/presentation/d/e/2PACX-1vTjWYyZhVQp38gvp7lmxTewmg5NWvKaZtBzNR_50Vq20MWDDR4bQihYWiMTJLx7v9Ez8fGJ7c2atZzJ/embed?start=true&loop=true&delayms=2000"
+    },
+
+    heyweb: {
+        title: "Hey Whizzy:Admin Website",
+        description: "Hey Whizzy's Admin website designed to manipulate data for the main kiosk.",
+        github: "https://github.com/Hir0su/Hey-Whizzy-Admin-Website",
+        slides: "https://docs.google.com/presentation/d/e/2PACX-1vRwTygF6g2NaWjaG9Juzk6_Oq26tUq9zwwrmn0g2lKdZiWbhFfMSSXgSoqW7cy03JNSmtRBVMRJOvqU/embed?start=true&loop=true&delayms=2000"
+    },
+
+    gpitch: {
+        title: "",
+        description: "",
+        github: "",
+        slides: "",
+    },
+
+    equake: {
+        title: "Earthquake Sensor",
+        description: "Arduino project",
+        github: "https://github.com/Hir0su/Earthquake-Detection",
+        slides: "https://docs.google.com/presentation/d/e/2PACX-1vSomMrzdWOyNpXbTnMn5FxOeF5CGRVlHSdg3GFmwSE51y1PRcuXmhcN8XQiQfz_Bjz2-AJRVh7AaRTG/embed?start=true&loop=true&delayms=2000",
+    },
+
+    ppttopdf: {
+        title: "PPT to PDF Module",
+        description: "It's a module created for a Web Application in my previous internship company. It is used to convert PPT files to PDF and be directly accessible to a new tab",
+        github: "https://github.com/andododo/PPT-to-PDF-Tool",
+        slides: "https://docs.google.com/presentation/d/e/2PACX-1vTVKUrO01NvUOWT8ZqSO5l_QvlubBxtnu1ZCmsH4df_gnWrVsjBDi0cIkVWlJF2JyUnB5yhT3E83gdQ/embed?start=true&loop=true&delayms=2000"
     }
+
+
 };
 
 document.querySelectorAll('.project-details').forEach(button => {
